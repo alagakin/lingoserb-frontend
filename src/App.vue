3d1e65a4bc111cebf12749f7266506c56c92e090
Alexandr Agakin <agakinalexandr@gmail.com>
2023-05-14 10:07:43 +0200

    Index, header prototype

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HomeView from './views/HomeView.vue';
-</script>
-
 <template>
+  <Header />
   <HomeView />
 </template>
+
+<script>
+import Header from './components/Header.vue';
+import HomeView from './views/HomeView.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    HomeView
+}
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="flex items-center justify-between bg-gray-200 py-4 px-6">
+        <div class="flex items-center">
+            <div class="mr-4">
+                <img src="logo.png" alt="Logo" class="h-8">
+            </div>
+            <a href="#" class="text-blue-600 font-semibold">Dictionary</a>
+        </div>
+        <div class="flex items-center">
+            <div class="mr-4">
+                <select class="p-2 border border-gray-300 rounded">
+                    <option value="en">English</option>
+                    <option value="es">Русский</option>
+                </select>
+            </div>
+            <a href="#" class="text-blue-600">John Doe</a> <!-- Replace with user's name -->
+            <a href="#" class="ml-2 text-gray-500">Login</a> <!-- Replace with login link -->
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
