2ca141dec47b4d44fbe6f4e266237517931d1203
Alexandr Agakin <agakinalexandr@gmail.com>
2023-05-14 21:58:49 +0200

    logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
     <header class="flex items-center justify-between bg-gray-200 py-4 px-6">
         <div class="flex items-center">
             <router-link to="/" class="text-blue-600 font-semibold">
-                <img src="logo.png" alt="Logo" class="h-8">
+                <img src="../assets/logo.png" alt="Logo" class="h-8 rounded-lg">
             </router-link>
             <router-link to="/dictionary" class="text-blue-600 font-semibold ml-4">Dictionary</router-link>
             <router-link to="/game" class="text-blue-600 font-semibold ml-4">Game</router-link>
