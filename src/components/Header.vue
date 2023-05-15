2b8ca2df493eea6408d8062dd033470ba3e9596a
Alexandr Agakin <agakinalexandr@gmail.com>
2023-05-15 09:54:23 +0200

    active link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,10 +4,9 @@
             <router-link to="/" class="text-blue-600 font-semibold">
                 <img src="../assets/logo.png" alt="Logo" class="h-8 rounded-lg">
             </router-link>
-            <router-link to="/dictionary" class="text-blue-600 font-semibold ml-4">Dictionary</router-link>
-            <router-link to="/game" class="text-blue-600 font-semibold ml-4">Game</router-link>
-            <router-link to="/my-words" class="text-blue-600 font-semibold ml-4">My words</router-link>
-
+            <router-link to="/dictionary" class="text-blue-600 font-semibold ml-4" exact active-class="underline">Dictionary</router-link>
+            <router-link to="/game" class="text-blue-600 font-semibold ml-4" exact active-class="underline">Game</router-link>
+            <router-link to="/my-words" class="text-blue-600 font-semibold ml-4" exact active-class="underline">My words</router-link>
         </div>
         <div class="flex items-center">
             <HeaderPersonal />
