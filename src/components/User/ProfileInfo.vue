9c7eb798eb242e912c5bae1f3f021ff09b6d97b3
Alexandr Agakin <agakinalexandr@gmail.com>
2023-06-06 09:46:20 +0200

    separated profile info in profile view

---
+++ src/components/User/ProfileInfo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="w-1/3 mb-12">
+        <h2 class="text-2xl text-center">About me</h2>
+        <div class="text-lg mt-6">
+          <p>Name</p>
+          <p>Joined at ....</p>
+          <p>Personal info</p>
+          <Logout />
+        </div>
+      </div>
+</template>
+  
+<script>
+import Logout from './Logout.vue';
+export default {
+    name: 'ProfileInfo',
+    components: { Logout }
+};
+</script>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,16 +1,10 @@
 <template>
   <div class="flex justify-between">
-    <Graph />
-    <!-- <div class="w-1/3">
-      <h2 class="text-2xl text-center">About me</h2>
-      <div class="text-lg mt-6">
-        <p>Name</p>
-        <p>Joined at ....</p>
-        <p>Personal info</p>
-        <Logout />
+    <div>
+      <ProfileInfo />
+      <Graph />
+    </div>
 
-      </div>
-    </div> -->
     <AchievementsList />
 
   </div>
@@ -18,11 +12,11 @@
 <script>
 import AchievementsList from '../components/User/AchievementsList.vue';
 import Graph from '../components/User/Graph.vue';
-import Logout from '../components/User/Logout.vue'
+import ProfileInfo from '../components/User/ProfileInfo.vue';
 
 
 export default {
   name: "Profile",
-  components: { AchievementsList, Logout, Graph }
+  components: { AchievementsList, Graph, ProfileInfo }
 }
 </script>
