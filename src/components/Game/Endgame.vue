55913f5094662970221434cd15495fc4fcff12e8
Alexandr Agakin <agakinalexandr@gmail.com>
2023-06-13 23:15:59 +0200

    endgame localization

--- src/components/Game/Endgame.vue
+++ src/components/Game/Endgame.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col justify-center items-center mt-6 text-gray-800">
-        <h1 class="text-5xl font-bold mb-4">Congratulations!</h1>
-        <p class="text-xl">Come in a few hours to repeat new words</p>
+        <h1 class="text-5xl font-bold mb-4">{{ $t('game.endgame.title') }}</h1>
+        <p class="text-xl">{{ $t('game.endgame.description') }}</p>
     </div>
 </template>
 <script>
