def3ef74b7089e39279bd4b54ab3b6f0841a7ca8
Alexandr Agakin <agakinalexandr@gmail.com>
2023-06-14 10:08:56 +0200

    not found page - localization

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,17 +1,16 @@
 <template>
-  <h1 class="text-4xl text-center text-white">404 Not found</h1>
+  <h1 class="text-4xl text-center ">{{ $t('notFound') }}</h1>
   <p class="text-center">
-    <router-link :to="{name: 'Home'}" class="text-white font-semibold ml-4 underline text-2xl ">Home</router-link>
+    <router-link :to="{name: 'Home'}" class="font-semibold ml-4 underline text-2xl ">{{ $t('header.home') }}</router-link>
 
   </p>
 
 </template>
 <script>
-
 export default {
     name: 'Not found',
-    created() {
-        document.tile = $t('notFound');
+    mounted() {
+        document.title = '404';
     }
 }
 </script>
