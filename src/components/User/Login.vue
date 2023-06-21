3adad5936e31d9cfa461a7b95a1d100bbd0efdf9
Alexandr Agakin <agakinalexandr@gmail.com>
2023-06-21 09:44:16 +0200

    login form localication

--- src/components/User/Login.vue
+++ src/components/User/Login.vue
@@ -28,7 +28,7 @@
                                 $t('auth.modal.login.login_field') }}</label>
                             <input type="text" name="username" id="username" v-model="username"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                                placeholder="My Username" required>
+                                :placeholder="$t('auth.modal.login.login_field_placeholder')" required>
                         </div>
                         <div>
                             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{
@@ -81,7 +81,7 @@
                                     </g>
                                 </g>
                             </svg>
-                            <span>Continue with Google</span>
+                            <span>{{ $t('auth.modal.login.google_auth_button') }}</span>
 
                         </button>
 
