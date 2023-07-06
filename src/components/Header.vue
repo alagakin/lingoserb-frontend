1c9b5b64e9c4842ffba471131afa0ebffa827b67
alex <agakinalexandr@gmail.com>
2023-07-06 21:01:37 +0200

    logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,9 @@
     <header class="flex items-center justify-between bg-white py-4 px-6">
         <div class="flex items-center">
             <router-link :to="{ name: 'Home' }" class="text-blue-600 font-semibold" exact active-class="underline">
-                {{ $t("header.home") }} </router-link>
+                <img :src="logo" class="h-12 border-2 rounded-full" alt=""> </router-link>
+
+
             <router-link :to="{ name: 'Dictionary' }" class="text-blue-600 font-semibold ml-4" exact
                 active-class="underline">{{ $t("header.dictionary") }}</router-link>
             <router-link :to="{ name: 'MyWords' }" class="text-blue-600 font-semibold ml-4" exact active-class="underline">
@@ -15,9 +17,15 @@
 </template>
 <script>
 import HeaderPersonal from './User/HeaderUser.vue'
+import logo from '@/assets/logo.png';
 
 export default {
     name: 'Header',
     components: { HeaderPersonal },
+    data() {
+        return {
+            logo: logo,
+        };
+    },
 }
 </script>
