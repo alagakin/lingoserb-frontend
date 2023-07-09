af9f79d1b61f262606d3e6b4dce790b27b61dda5
alex <agakinalexandr@gmail.com>
2023-07-09 18:32:42 +0200

    better link color

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,15 @@
 <template>
     <header class="flex items-center justify-between bg-white py-4 px-6">
-        <div class="flex items-center">
-            <router-link :to="{ name: 'Home' }" class="text-blue-600 font-semibold" exact active-class="underline">
+        <div class="flex items-center text-xl">
+            <router-link :to="{ name: 'Home' }">
                 <img :src="logo" class="h-12 border-2 rounded-full" alt=""> </router-link>
 
-            <router-link :to="{ name: 'Topics' }" class="text-blue-600 font-semibold ml-4" exact
-                active-class="underline">{{ $t("header.topics") }}</router-link>
+            <router-link :to="{ name: 'Topics' }" class="text-blue-400 font-semibold ml-6" exact
+                active-class="text-blue-600">{{ $t("header.topics") }}</router-link>
 
-            <router-link :to="{ name: 'Dictionary' }" class="text-blue-600 font-semibold ml-4" exact
-                active-class="underline">{{ $t("header.dictionary") }}</router-link>
-            <router-link :to="{ name: 'MyWords' }" class="text-blue-600 font-semibold ml-4" exact active-class="underline">
+            <router-link :to="{ name: 'Dictionary' }" class="text-blue-400 font-semibold ml-6" exact
+                active-class="text-blue-600">{{ $t("header.dictionary") }}</router-link>
+            <router-link :to="{ name: 'MyWords' }" class="text-blue-400 font-semibold ml-6" exact active-class="text-blue-600">
                 {{ $t("header.myWords") }}</router-link>
         </div>
         <div class="flex items-center">
