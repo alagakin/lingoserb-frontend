9b0191b7709ce39742518ddf2a3b92aeb66904f2
alex <agakinalexandr@gmail.com>
2023-07-09 18:25:37 +0200

    topic list - removed unnecessary links, added learn button

--- src/components/Topic/Slide.vue
+++ src/components/Topic/Slide.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 px-4 mb-8">
         <div class="bg-white rounded-lg shadow p-4">
-            <router-link :to="{ name: 'DetailTopic', params: { id: subtopic.id } }">
+            <router-link :to="{ name: 'DetailTopic', params: { id: subtopic.id } }" v-if="subtopic.words_count">
                 <img v-show="subtopic.picture" :src="subtopic.picture" alt="Block Picture"
                     class="w-full h-40 object-cover mb-4 rounded-lg">
                 <div 
@@ -11,7 +11,26 @@
                     >
                 </div>
             </router-link>
-            <h3 class="text-lg font-bold mb-2">{{ getTitle(subtopic) }}</h3>
+            <div v-else>
+                <img v-show="subtopic.picture" :src="subtopic.picture" alt="Block Picture"
+                    class="w-full h-40 object-cover mb-4 rounded-lg">
+                <div 
+                    v-show="!subtopic.picture" 
+                    class="w-full h-40 object-cover mb-4 rounded-lg"
+                    :class="getBackground(subtopic.id)"
+                    >
+                </div>
+            </div>
+
+            <div class="flex justify-between">
+                <h3 class="text-lg font-bold mb-2">{{ getTitle(subtopic) }}</h3>
+                <router-link :to="{ name: 'Game', params: { id: subtopic.id } }">
+                <div 
+                    v-if="subtopic.learned_percent < 100 && subtopic.words_count > 0"
+                    class="mr-2 cursor-pointer bg-blue-400 pt-1 pb-1 pl-2 pr-2 text-white rounded-xl"
+                >{{ $t('learning.start') }}</div>
+                </router-link>
+            </div>
             <p class="text-gray-500 mb-4">{{ $t('topic.words.counter', { count: subtopic.words_count }) }}</p>
             <TopicProgress :percent="subtopic.learned_percent" />
         </div>
